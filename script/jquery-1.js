fda99148c7bb7ab5a9668ed591e606b88308cdd8
Thai RuBi <thairubi1997@gmail.com>
2019-10-01 16:37:13 +0700

    update 4:36pm 10/1/2019

--- script/jquery-1.js
+++ script/jquery-1.js
@@ -55,6 +55,18 @@ $('body').on("click", ".dropdown-menu", function (e) {
         var newVal = parseFloat(oldValue) + 1;   
     $parent.find('.input').val(newVal);
  });
+  $("#show_hide_password a").on('click', function(event) {
+        event.preventDefault();
+        if($('#show_hide_password input').attr("type") == "text"){
+            $('#show_hide_password input').attr('type', 'password');
+            $('#show_hide_password i').addClass( "fa-eye-slash" );
+            $('#show_hide_password i').removeClass( "fa-eye" );
+        }else if($('#show_hide_password input').attr("type") == "password"){
+            $('#show_hide_password input').attr('type', 'text');
+            $('#show_hide_password i').removeClass( "fa-eye-slash" );
+            $('#show_hide_password i').addClass( "fa-eye" );
+        }
+    });
 }
 )
 
