27efe0642e3f10f0864da1ea9559bb66ae106fbc
Thai RuBi <thairubi1997@gmail.com>
2019-10-03 14:33:09 +0700

    233

--- script/menu.js
+++ script/menu.js
@@ -16,16 +16,27 @@ function openCart() {
 function closeCart() {
   document.getElementById("cart-mobi-header").style.width = "0";
 }
-function check() {
+function checkmail() {
 	var mail = document.getElementById('email').value;
-	var pass = document.getElementById('password').value.length;
     if (mail=="") {
         document.getElementById('z-email').innerHTML = "<span>Email address cannot be empty</span>";
         document.getElementById("email").classList.add("is-invalid");
     }
-    if (pass<8){
-    	document.getElementById('z-password').innerHTML = "<span>Your password must be longer than 8 characters</span>";
-        document.getElementById("password").classList.add("is-invalid");
+    else{
+      document.getElementById('z-email').innerHTML = "";
+      document.getElementById("email").classList.remove("is-invalid");
+    }
+    
+}
+function checkpass() {
+  var pass = document.getElementById('password').value.length;
+  if (pass<8){
+      document.getElementById('z-password').innerHTML = "<span>Your password must be longer than 8 characters</span>";
+      document.getElementById("password").classList.add("is-invalid");
+    }
+    else{
+      document.getElementById('z-password').innerHTML ="";
+      document.getElementById("password").classList.remove("is-invalid");
     }
 }
 function myFunction() {
