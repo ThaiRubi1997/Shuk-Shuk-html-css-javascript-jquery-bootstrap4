708e3da395b5dc1b59073233953bd0646facf320
Thai RuBi <thairubi1997@gmail.com>
2019-12-03 10:38:55 +0700

    1038

--- seller/script/checked.js
+++ seller/script/checked.js
@@ -1,10 +1,12 @@
 $(function(){
 	$('#check-1').click(function () {
-		$('#check-2').removeClass('checked')
-        $('#check-1').addClass('checked');
+		if ($('#input-2').is(":checked")) {
+			 $('#input-2').removeAttr("checked");
+		}
     })
     $('#check-2').click(function () {
-		$('#check-1').removeClass('checked')
-        $('#check-2').addClass('checked');
+		if ($('#input-1').is(":checked")) {
+			$('#input-1').removeAttr("checked");
+		}
     })
 })
