0803da6e32a1817231d982cd8c41f46507c0760c
Thai RuBi <thairubi1997@gmail.com>
2019-12-07 09:33:30 +0700

    933

--- seller/script/seller-add-product.js
+++ seller/script/seller-add-product.js
@@ -1,5 +1,5 @@
 $(function(){
-	 $('.on-off').click(function(){
+	$('.on-off').click(function(){
 		if ($('.on-off').hasClass("on-show"))
 			{$('.on-off').removeClass('on-show');}
  		else{
@@ -7,5 +7,6 @@ $(function(){
  		}
  		if($('.wrap-parameter').hasClass('d-none')){$('.wrap-parameter').removeClass('d-none')}
  		else{$('.wrap-parameter').addClass('d-none')}
-    })
+    });
+
 });
